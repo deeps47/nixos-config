cb77c4fe6377d1e811af10ed323e21f740abdc9e
deeps47 <dy6579@srmist.edu.in>
2023-08-20 01:02:11 +0530

    added polkit & gnome-keyring

--- hosts/bankai/default.nix
+++ hosts/bankai/default.nix
@@ -104,6 +104,35 @@
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Iosevka" ]; })
   ];
+  
+  boot.initrd.systemd.dbus.enable = true;
+
+  programs.dconf.enable = true;
+  
+  services.gnome.gnome-keyring.enable = true;
+
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
+  security.polkit = {
+    enable = true;
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
 
   # TODO: Set your hostname
   networking.hostName = "bankai";
