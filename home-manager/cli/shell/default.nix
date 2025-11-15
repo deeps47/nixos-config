c04456dc89b48d4747e706e34953993ecac844df
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2025-11-15 07:06:51 +0530

    starting fresh

--- home-manager/cli/shell/default.nix
+++
@@ -1,5 +0,0 @@
-_: {
-
-  imports = [ ./zsh ./starship ];
-
-}
--- home-manager/hyprland/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
-
-hyprland-autoname-workspaces -c $HOME/config/home-manager/hyprland/waybar/autoname.toml &
-
-wlr-randr --output eDP-1 --mode 1920x1080 --scale 1
