59ef13f2ed88e5abac28694b42b63bc774cebdcf
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2025-11-19 08:53:52 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ echo "[+] Starting NixOS flake deployment..."
 
 mkdir -p /mnt
 mkdir -p /boot
-mkdir -p /etc/nixos/nixos-dotfiles
-
-cd /etc/nixos
+mkdir -p /home/goku/nixos-dotfiles
 
 # Run nixos-generate-config command
 sudo nixos-generate-config || { echo "Failed to generate hardware configuration"; exit 1; }
 
-rm /etc/nixos/configuration.nix
+sudo rm /etc/nixos/configuration.nix
 
-mv nixos-config/* /etc/nixos/nixos-dotfiles
+sudo mv nixos-dotfiles/* /etc/nixos/
 
 # Rebuild NixOS configuration
 sudo nixos-rebuild switch --flake /etc/nixos#nixos-btw || { echo "Failed to rebuild NixOS configuration"; exit 1; }
+
+sudo mv /etc/nixos/* /home/goku/nixos-dotfiles/
 
 echo "Script completed successfully."
