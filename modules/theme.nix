c9a297c04350126ef116815e95be93800fd759cf
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2025-12-09 00:02:28 +0530

    added dark theme

--- modules/theme.nix
+++ modules/theme.nix
@@ -6,35 +6,49 @@
     gnome-themes-extra
   ];
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+  
   gtk = {
     enable = true;
-    theme.name = "Adwaita";
-    iconTheme.name = "Adwaita";
+    theme.name = "Adwaita-dark";
+    iconTheme.name = "Adwaita-dark";
     cursorTheme = {
-      name = "Adwaita";
+      name = "Adwaita-dark";
       package = pkgs.adwaita-icon-theme;
       size = 24;
     };
   };
 
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
+  };
+  
   xdg.configFile."gtk-3.0/settings.ini".text = ''
     [Settings]
-    gtk-theme-name=Adwaita
-    gtk-icon-theme-name=Adwaita
-    gtk-cursor-theme-name=Adwaita
+    gtk-theme-name=Adwaita-dark
+    gtk-icon-theme-name=Adwaita-dark
+    gtk-cursor-theme-name=Adwaita-dark
     gtk-cursor-theme-size=24
+    gtk-application-prefer-dark-theme=1
   '';
 
   xdg.configFile."gtk-4.0/settings.ini".text = ''
     [Settings]
-    gtk-theme-name=Adwaita
-    gtk-icon-theme-name=Adwaita
-    gtk-cursor-theme-name=Adwaita
+    gtk-theme-name=Adwaita-dark
+    gtk-icon-theme-name=Adwaita-dark
+    gtk-cursor-theme-name=Adwaita-dark
     gtk-cursor-theme-size=24
+    gtk-application-prefer-dark-theme=1
   '';
 
   home.sessionVariables = {
-    XCURSOR_THEME = "Adwaita";
+    XCURSOR_THEME = "Adwaita-dark";
     XCURSOR_SIZE = "24";
   };
 }
